d98d0558f277903498fd71628ece694d2d56ae55
Yasmin <72038787+yazz18@users.noreply.github.com>
2020-10-05 15:22:54 -0500

    Área
    Se definió la constante y variable para obtener área de un circulo.

--- introduction/area.c
+++ introduction/area.c
@@ -1,11 +1,14 @@
-#include<stdio.h>
+#include<stdio.h> //Input Output
 #include<math.h>
-#define PI 3.1416
+
+#define PI 3.1416 //Definiendo una constante 
 
 int main(){
-	float area, radio;
-	radio = 5;
-	area = PI * pow(5,2);
-	printf("Area\n");
-	printf("%s%f\n\n", "Area de Circulo con radio 5:", area);
+	float area, radio; //declarando variables de tipo constante 
+	radio = 5; //asignando valor a variable radio 
+	area = PI * pow(5,2); //signando redultado de la operación a variable 
+	printf("Area\n"); //imprimiendo titulo 
+	//imprimiendo resultado 
+	printf("Area de Circulo con radio 5: %f", area);
+	return 0;
 }
